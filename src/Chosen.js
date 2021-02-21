db18d4eb97b0434031aa70ab979c3d8a3247c2d4
LauraToddCode <laura20todd@gmail.com>
2021-02-21 13:20:39 +0000

    Child components and stylesheet created

---
+++ src/Chosen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Chosen() {
+    return (
+        <div id="chosenContainer">
+            <p>Chosen option:</p>
+
+        </div>
+    )
+}
+
+export default Chosen;
---
+++ src/Dropdown.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dropdown() {
+    return (
+        <select>
+            <option value="Canada">Canada</option>
+            <option value="Canada">Italy</option>
+            <option value="Canada">Japan</option>
+            <option value="Canada">New Zealand</option>
+        </select>
+    )
+}
+
+export default Dropdown;
