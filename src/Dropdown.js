bbc39563fb168a60ce2a8ed28e765251a5b49d59
LauraToddCode <laura20todd@gmail.com>
2021-02-21 14:45:01 +0000

    Update Dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,14 +1,33 @@
 import React from "react";
 
 class Dropdown extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            chosen: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            chosen: event.target.value
+        })
+    }
+
     render() {
         return (
-            <select>
+            <div>
+            <select onChange={this.handleChange}>
+                <option value="">Choose Destination</option>
                 <option value="Canada">Canada</option>
-                <option value="Canada">Italy</option>
-                <option value="Canada">Japan</option>
-                <option value="Canada">New Zealand</option>
+                <option value="Italy">Italy</option>
+                <option value="Japan">Japan</option>
+                <option value="New Zealand">New Zealand</option>
             </select>
+            <p>{this.state.chosen}</p>
+            </div>
+
         )
     }
 }
